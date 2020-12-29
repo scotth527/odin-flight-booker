110f3a2bb0c384e7eafcd47fe772e25d5097bf6b
scotth527 <scottyh527@gmail.com>
2020-12-29 17:11:06 -0500

    installed livereload guard

--- Gemfile
+++ Gemfile
@@ -34,6 +34,9 @@ gem 'jquery-rails'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'guard-livereload', '~> 2.5', require: false
+  gem 'guard'
+  gem "rack-livereload"
 end
 
 group :development do
--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,25 +1,30 @@
 class FlightsController < ApplicationController
     def index
         @flights = Flight.all
-        @from_airports = Flight.all.uniq{ |x|
-            x.from_airport.airport_code
+
+        @available_airports = Airport.all.uniq{ |x| x.airport_code }
+        .map { |airport|
+            [ airport.airport_code , airport.id]
         }
-        .map { |flight|
-            [ flight.from_airport.airport_code , flight.from_airport_id]
-        }
-        @to_airports = Flight.all.uniq{ |x|
-            x.to_airport.airport_code
-        }
-        .map { |flight|
-            [ flight.to_airport.airport_code , flight.to_airport_id]
-        }
+        # @from_airports = Flight.all.uniq{ |x|
+        #     x.from_airport.airport_code
+        # }
+        # .map { |flight|
+        #     [ flight.from_airport.airport_code , flight.from_airport_id]
+        # }
+        # @to_airports = Flight.all.uniq{ |x|
+        #     x.to_airport.airport_code
+        # }
+        # .map { |flight|
+        #     [ flight.to_airport.airport_code , flight.to_airport_id]
+        # }
 
         @search_results = []
 
         if params
             p "These are my params"
             p params
-            @search_results = Flight.find_by(from_airport_id: params[:from_airport], to_airport_id: params[:to_aiport])
+            @search_results = Flight.where(from_airport_id: params[:from_airport], to_airport_id: params[:to_aiport])
             p "Search Results"
             p @search_results
         else
