9347d209dbb09eaa630f9eb85cf63c36cf5de2bc
scotth527 <scottyh527@gmail.com>
2020-12-29 19:46:43 -0500

    got the search for the from and to airports to work, worked on displaying the search results

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,33 +3,22 @@ class FlightsController < ApplicationController
         @flights = Flight.all
 
         @available_airports = Airport.all.uniq{ |x| x.airport_code }
-        .map { |airport|
-            [ airport.airport_code , airport.id]
-        }
-        # @from_airports = Flight.all.uniq{ |x|
-        #     x.from_airport.airport_code
-        # }
-        # .map { |flight|
-        #     [ flight.from_airport.airport_code , flight.from_airport_id]
-        # }
-        # @to_airports = Flight.all.uniq{ |x|
-        #     x.to_airport.airport_code
-        # }
-        # .map { |flight|
-        #     [ flight.to_airport.airport_code , flight.to_airport_id]
-        # }
+        .map { |airport| [ airport.airport_code , airport.id]}
 
-        @search_results = []
+        @passengers = [[1,1],[2,2],[3,3],[4,4]]
 
-        if params
+        if params[:search]
             p "These are my params"
             p params
-            @search_results = Flight.where(from_airport_id: params[:from_airport], to_airport_id: params[:to_aiport])
+            @search_results = Flight.where(
+                    from_airport_id:  params[:from_airport],
+                    to_airport: params[:to_airport]
+            )
+
             p "Search Results"
             p @search_results
-        else
-            p "No params available"
-            @search_results
+            p "Length of results"
+            p @search_results.length()
         end
     end
 
